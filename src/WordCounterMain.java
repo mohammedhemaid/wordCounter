f4a3debd03dede174ff8ee4b2ea8040e5f8c49b8
Moham <m7md972013@gmail.com>
2019-10-29 18:11:47 +0200

    Initial commit

---
+++ src/WordCounterMain.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class WordCounterMain {
+
+    private static final String WORD_PER_MINUTE = "Words Per minute: ";
+    private static int bigFileMinutes = 42;
+    private static int smallFileMinutes = 22;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReaderBigFile = readFile("bigFile.txt");
+        BufferedReader bufferedReaderSmallFile = readFile("smallFile.txt");
+
+        int bigWordPerMinute = getBigWordPerMinute(bufferedReaderBigFile, bigFileMinutes);
+
+        int smallWordPerMinute = getBigWordPerMinute(bufferedReaderSmallFile, smallFileMinutes);
+
+
+        System.out.println("average " + (smallWordPerMinute + bigWordPerMinute) / 2);
+    }
+
+    private static int getBigWordPerMinute(BufferedReader bufferedReaderBigFile, int minutes) throws IOException {
+        int counterBig = getCounter(bufferedReaderBigFile);
+        int bigWordPerMinute = counterBig / minutes;
+        System.out.println(WORD_PER_MINUTE + bigWordPerMinute);
+        return bigWordPerMinute;
+    }
+
+    private static BufferedReader readFile(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        return new BufferedReader(new InputStreamReader(fis));
+    }
+
+    private static int getCounter(BufferedReader bufferedReader) throws IOException {
+        int counter = 0;
+
+        FileOutputStream fos = new FileOutputStream("Out.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fos));
+
+        for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+            String withoutSpaces = line.replaceAll("\\s+", " ");
+            if (line.length() != 0 && line.length() != 1 && !line.matches(".*\\d.*")) {
+                String[] arraysOfString = withoutSpaces.split(" ");
+                counter += arraysOfString.length;
+                bufferedWriter.write(withoutSpaces + "\n");
+            }
+
+        }
+        return counter;
+    }
+}
